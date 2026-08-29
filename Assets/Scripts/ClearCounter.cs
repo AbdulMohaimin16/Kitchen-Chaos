018a87ddc79c65dae121eeeb95a908bcf2cabefc
Abdul Mohaimin Adnan <abdulmohaimin16@gmail.com>
2026-08-29 20:13:23 +0500

    Updated the ClearCounter script with the logic of instantiating kitchen objects.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,10 +1,19 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
-    
+    [SerializeField] private KitchenObjectScriptableObject kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact()
     {
         Debug.Log("Interact");
+      
+        GameObject kitchenObjectGameObject = Instantiate(kitchenObjectSO.prefab, counterTopPoint);;
+        kitchenObjectGameObject.transform.localPosition = Vector3.zero;
+
+        Debug.Log("Instantiated: " + kitchenObjectGameObject.GetComponent<GetKitchenObjectType>().GetKitchenObjectSO().objectName);
     }
 }
