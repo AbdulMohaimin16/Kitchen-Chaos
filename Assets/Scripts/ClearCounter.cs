b0de0cd0a67c709c6decfc63df8dd3f63fc9c6c2
Abdul Mohaimin Adnan <abdulmohaimin16@gmail.com>
2026-08-31 14:51:18 +0500

    Added testing logic for transferring kitchen objects from one position to another.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,14 +6,59 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private KitchenObjectScriptableObject kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
+    
+    [SerializeField] private ClearCounter secondClearCounter;
+    
+    [SerializeField] private bool testing;
+
+    private GetKitchenObjectType getkitchenObjectType;
+
+    private void Update()
+    {
+        if(testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if(getkitchenObjectType != null)
+            {
+                getkitchenObjectType.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
 
     public void Interact()
     {
-        Debug.Log("Interact");
-      
-        GameObject kitchenObjectGameObject = Instantiate(kitchenObjectSO.prefab, counterTopPoint);;
-        kitchenObjectGameObject.transform.localPosition = Vector3.zero;
+      if(getkitchenObjectType == null)
+        {
+            GameObject kitchenObjectGameObject = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectGameObject.transform.GetComponent<GetKitchenObjectType>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(getkitchenObjectType.GetClearCounter());
+        }  
+    }
 
-        Debug.Log("Instantiated: " + kitchenObjectGameObject.GetComponent<GetKitchenObjectType>().GetKitchenObjectSO().objectName);
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(GetKitchenObjectType getKitchenObjectType)
+    {
+        this.getkitchenObjectType = getKitchenObjectType;
+    }
+
+    public GetKitchenObjectType GetKitchenObject()
+    {
+        return getkitchenObjectType;
+    }
+
+    public void ClearKitchenObject()
+    {
+        getkitchenObjectType = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return getkitchenObjectType != null;
     }
 }
