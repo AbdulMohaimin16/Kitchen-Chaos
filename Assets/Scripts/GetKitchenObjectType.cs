dd264ca32e8c98614605e509f586e5f042ea1b72
Abdul Mohaimin Adnan <abdulmohaimin16@gmail.com>
2026-08-31 14:51:40 +0500

    Logic for knowing what counter holds the kitchen object

--- Assets/Scripts/GetKitchenObjectType.cs
+++ Assets/Scripts/GetKitchenObjectType.cs
@@ -4,8 +4,35 @@ public class GetKitchenObjectType : MonoBehaviour
 {
     [SerializeField] private KitchenObjectScriptableObject kitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
     public KitchenObjectScriptableObject GetKitchenObjectSO()
     {
         return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if(this.clearCounter != null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
+        
+        this.clearCounter = clearCounter;
+
+        if(clearCounter.HasKitchenObject())
+        {
+            Debug.LogError("ClearCounter already has a kitchen object!");
+        }
+
+        clearCounter.SetKitchenObject(this);
+
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
