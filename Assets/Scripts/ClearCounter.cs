0a58ff60505eec622b44c4b5375d96c0851c29cf
Abdul Mohaimin Adnan <abdulmohaimin16@gmail.com>
2026-09-01 16:34:00 +0500

    Created 2 prefab variants from the base counter prefab for the clear and container counters.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,48 +2,12 @@ using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour, IKitchenObjectParent
+public class ClearCounter : BaseCounter
 {
     [SerializeField] private KitchenObjectScriptableObject kitchenObjectSO;
-    [SerializeField] private Transform counterTopPoint;
 
-    private GetKitchenObjectType getKitchenObjectType;
-
-    public void Interact(Player player)
+    public override void Interact(Player player)
     {
-      if(getKitchenObjectType == null)
-        {
-            GameObject kitchenObjectGameObject = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-            kitchenObjectGameObject.transform.GetComponent<GetKitchenObjectType>().SetKitchenObjectParent(this);
-        }
-        else
-        {
-            getKitchenObjectType.SetKitchenObjectParent(player);
-        }  
-    }
-
-    public Transform GetKitchenObjectFollowTransform()
-    {
-        return counterTopPoint;
-    }
-
-    public void SetKitchenObject(GetKitchenObjectType getKitchenObjectType)
-    {
-        this.getKitchenObjectType = getKitchenObjectType;
-    }
-
-    public GetKitchenObjectType GetKitchenObject()
-    {
-        return getKitchenObjectType;
-    }
-
-    public void ClearKitchenObject()
-    {
-        getKitchenObjectType = null;
-    }
-
-    public bool HasKitchenObject()
-    {
-        return getKitchenObjectType != null;
+        Debug.Log("Will be implementing interaction with clear counter");
     }
 }
