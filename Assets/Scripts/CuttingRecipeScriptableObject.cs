8a243322a2fd2929fff9f17f96b1505a30eb93c0
AbdulMohaimin16 <abdulmohaimin16@gmail.com>
2026-09-16 20:41:07 +0500

    New variable for the max cuts.

--- Assets/Scripts/CuttingRecipeScriptableObject.cs
+++ Assets/Scripts/CuttingRecipeScriptableObject.cs
@@ -5,4 +5,6 @@ public class CuttingRecipeScriptableObject : ScriptableObject
 {
     public KitchenObjectScriptableObject input;
     public KitchenObjectScriptableObject output;
+
+    public int cuttingProgressMax;
 }
