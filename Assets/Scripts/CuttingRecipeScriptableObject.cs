6a2cf7114609a17d73388d62614eab8d7be73142
AbdulMohaimin16 <abdulmohaimin16@gmail.com>
2026-09-16 11:30:23 +0500

    New scriptable object for the recipes

---
+++ Assets/Scripts/CuttingRecipeScriptableObject.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CuttingRecipe", menuName = "ScriptableObjects/CuttingRecipeScriptableObject", order = 1)]
+public class CuttingRecipeScriptableObject : ScriptableObject
+{
+    public KitchenObjectScriptableObject input;
+    public KitchenObjectScriptableObject output;
+}
