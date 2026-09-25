748f47d5d22a1f60cc97003eab1e95ae43d6c481
AbdulMohaimin16 <abdulmohaimin16@gmail.com>
2026-09-25 11:30:00 +0500

    Added and event for when the plate gets added with an object.

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using System;
 
 public class PlateKitchenObject : GetKitchenObjectType
 {
+    public event EventHandler<OnIngredientEventArgs> OnIngredientAdded;
+    public class OnIngredientEventArgs : EventArgs
+    {
+        public KitchenObjectScriptableObject kitchenObjectSO;
+    }
+
 
     [SerializeField] private List<KitchenObjectScriptableObject> validKitchenObjectSO;
     private List<KitchenObjectScriptableObject> kitchenObjectSOList;
@@ -21,6 +29,11 @@ public class PlateKitchenObject : GetKitchenObjectType
         if (!kitchenObjectSOList.Contains(kitchenObjectSO))
         {
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientEventArgs
+            {
+                kitchenObjectSO = kitchenObjectSO
+            });
             return true;
         }
         return false;
